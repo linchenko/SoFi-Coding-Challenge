d0eb32ef38853a8008884c8e60704c3f9b8352f7
Levi Linchenko <levi.r.lawson@gmail.com>
2018-11-20 10:11:58 -0700

    First Commit

---
+++ SoFi Coding Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoFi Coding Challenge
+//
+//  Created by Levi Linchenko on 20/11/2018.
+//  Copyright © 2018 Levi Linchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
